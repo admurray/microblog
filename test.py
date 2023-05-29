0b1cfd7546fe27a369615e2090d6c69dc5f9ab54
admurray <adityamurray@yahoo.com>
2023-05-28 20:18:26 -0400

    refactor app

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
-import os
-import unittest
-
+#!/usr/bin/env python
 from datetime import datetime, timedelta
-
-from app import app, db
+import unittest
+from app import create_app, db
 from app.models import User, Post
+from config import Config
 
-os.environ['DATABASE_URL'] = 'sqlite://'
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
 
 class UserModelCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.app_context = app.app_context()
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
@@ -22,7 +26,7 @@ class UserModelCase(unittest.TestCase):
     def test_password_hashing(self):
         u = User(username='susan')
         u.set_password('cat')
-        self.assertFalse((u.check_password('dog')))
+        self.assertFalse(u.check_password('dog'))
         self.assertTrue(u.check_password('cat'))
 
     def test_avatar(self):
